b278211685167a2eb2c746d8fc580bba6041ba57
Kijin Kim <msrndsy@gmail.com>
2022-06-04 16:47:02 +0900

    Add project files.

---
+++ VolatileTextEditor/MainForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace VolatileTextEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.DeleteProgressBar = new System.Windows.Forms.ProgressBar();
+            this.DebugLabel = new System.Windows.Forms.Label();
+            this.TimecheckTimer = new System.Windows.Forms.Timer(this.components);
+            this.StartButton = new System.Windows.Forms.Button();
+            this.DeleteTimeSetTextBox = new System.Windows.Forms.TextBox();
+            this.TimeSetHelpLabel1 = new System.Windows.Forms.Label();
+            this.TimeScaleIndicateLabel = new System.Windows.Forms.Label();
+            this.FontSelectButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainRichTextBox
+            // 
+            this.MainRichTextBox.Font = new System.Drawing.Font("Malgun Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MainRichTextBox.Location = new System.Drawing.Point(24, 54);
+            this.MainRichTextBox.Name = "MainRichTextBox";
+            this.MainRichTextBox.Size = new System.Drawing.Size(957, 624);
+            this.MainRichTextBox.TabIndex = 0;
+            this.MainRichTextBox.Text = "";
+            this.MainRichTextBox.TextChanged += new System.EventHandler(this.MainRichTextBox_TextChanged);
+            // 
+            // DeleteProgressBar
+            // 
+            this.DeleteProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DeleteProgressBar.Location = new System.Drawing.Point(25, 698);
+            this.DeleteProgressBar.Name = "DeleteProgressBar";
+            this.DeleteProgressBar.Size = new System.Drawing.Size(956, 32);
+            this.DeleteProgressBar.TabIndex = 1;
+            // 
+            // DebugLabel
+            // 
+            this.DebugLabel.AutoSize = true;
+            this.DebugLabel.Location = new System.Drawing.Point(696, 681);
+            this.DebugLabel.Name = "DebugLabel";
+            this.DebugLabel.Size = new System.Drawing.Size(60, 25);
+            this.DebugLabel.TabIndex = 2;
+            this.DebugLabel.Text = "label1";
+            this.DebugLabel.Visible = false;
+            // 
+            // TimecheckTimer
+            // 
+            this.TimecheckTimer.Interval = 1;
+            this.TimecheckTimer.Tick += new System.EventHandler(this.TimecheckTimer_Tick);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.StartButton.Location = new System.Drawing.Point(754, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(74, 36);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // DeleteTimeSetTextBox
+            // 
+            this.DeleteTimeSetTextBox.Location = new System.Drawing.Point(553, 15);
+            this.DeleteTimeSetTextBox.Name = "DeleteTimeSetTextBox";
+            this.DeleteTimeSetTextBox.Size = new System.Drawing.Size(153, 31);
+            this.DeleteTimeSetTextBox.TabIndex = 4;
+            this.DeleteTimeSetTextBox.Text = "5000";
+            this.DeleteTimeSetTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // TimeSetHelpLabel1
+            // 
+            this.TimeSetHelpLabel1.AutoSize = true;
+            this.TimeSetHelpLabel1.Location = new System.Drawing.Point(25, 18);
+            this.TimeSetHelpLabel1.Name = "TimeSetHelpLabel1";
+            this.TimeSetHelpLabel1.Size = new System.Drawing.Size(518, 25);
+            this.TimeSetHelpLabel1.TabIndex = 5;
+            this.TimeSetHelpLabel1.Text = "All text will be deleted when you stop writing... Waiting time : ";
+            // 
+            // TimeScaleIndicateLabel
+            // 
+            this.TimeScaleIndicateLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TimeScaleIndicateLabel.AutoSize = true;
+            this.TimeScaleIndicateLabel.Location = new System.Drawing.Point(716, 18);
+            this.TimeScaleIndicateLabel.Name = "TimeScaleIndicateLabel";
+            this.TimeScaleIndicateLabel.Size = new System.Drawing.Size(36, 25);
+            this.TimeScaleIndicateLabel.TabIndex = 6;
+            this.TimeScaleIndicateLabel.Text = "ms";
+            // 
+            // FontSelectButton
+            // 
+            this.FontSelectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.FontSelectButton.Location = new System.Drawing.Point(834, 12);
+            this.FontSelectButton.Name = "FontSelectButton";
+            this.FontSelectButton.Size = new System.Drawing.Size(72, 36);
+            this.FontSelectButton.TabIndex = 7;
+            this.FontSelectButton.Text = "FONT";
+            this.FontSelectButton.UseVisualStyleBackColor = true;
+            this.FontSelectButton.Click += new System.EventHandler(this.FontSelectButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.Location = new System.Drawing.Point(912, 12);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(69, 36);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "SAVE";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 753);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.FontSelectButton);
+            this.Controls.Add(this.DeleteTimeSetTextBox);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.TimeScaleIndicateLabel);
+            this.Controls.Add(this.TimeSetHelpLabel1);
+            this.Controls.Add(this.DebugLabel);
+            this.Controls.Add(this.DeleteProgressBar);
+            this.Controls.Add(this.MainRichTextBox);
+            this.Name = "MainForm";
+            this.Text = "VolatileTextEditor";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RichTextBox MainRichTextBox;
+        private ProgressBar DeleteProgressBar;
+        private Label DebugLabel;
+        private System.Windows.Forms.Timer TimecheckTimer;
+        private Button StartButton;
+        private TextBox DeleteTimeSetTextBox;
+        private Label TimeSetHelpLabel1;
+        private Label TimeScaleIndicateLabel;
+        private Button FontSelectButton;
+        private Button SaveButton;
+    }
+}
---
+++ VolatileTextEditor/MainForm.cs
@@ -0,0 +1,169 @@
+using System.Diagnostics;
+using System.Text;
+
+namespace VolatileTextEditor
+{
+    public partial class MainForm : Form
+    {
+        DateTimeOffset timeOffset;
+        private int deleteInterval;
+        private long deleteTick;
+
+        FontDialog fontDialog;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            try
+            {
+                deleteInterval = Int32.Parse(DeleteTimeSetTextBox.Text);
+            }
+            catch
+            {
+                deleteInterval = 5000;
+            }
+            deleteTick = GetCurrentTick() + deleteInterval * 10000;
+
+            fontDialog = new FontDialog();
+            ResizeComponent();
+        }
+
+        private long GetCurrentTick()
+        {
+            timeOffset = DateTimeOffset.Now;
+            return timeOffset.UtcTicks;
+        }
+
+        private void ResizeComponent()
+        {
+            ResizeTextbox();
+            ResizeDeleteProgreeBar();
+
+            DeleteTimeSetTextBox.Width = TimeScaleIndicateLabel.Left - TimeSetHelpLabel1.Right - 10;
+        }
+
+        private void ResizeDeleteProgreeBar()
+        {
+            DeleteProgressBar.Left = 20;
+            DeleteProgressBar.Top = this.Height - 30;
+            DeleteProgressBar.Width = this.Size.Width - 60;
+        }
+
+        private void ResizeTextbox()
+        {
+            MainRichTextBox.Left = 20;
+            MainRichTextBox.Width = this.Size.Width - 60;
+            MainRichTextBox.Width = this.Size.Width - 60;
+            MainRichTextBox.Height = this.Size.Height - (100 + MainRichTextBox.Top);
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            ResizeComponent();
+        }
+
+        private void TimecheckTimer_Tick(object sender, EventArgs e)
+        {
+            long current_tick = GetCurrentTick();
+
+            DebugLabel.Text = (deleteTick - current_tick).ToString();
+
+            if (deleteTick >= current_tick)
+            {
+                long diff_tick = deleteTick - current_tick;
+                DeleteProgressBar.Value = (int)((diff_tick * 100) / (deleteInterval * 10000));
+            }
+            else
+            {
+                DeleteProgressBar.Value = 0;
+                deleteTick = GetCurrentTick() + deleteInterval * 10000;
+                MainRichTextBox.Text = "";
+            }
+        }
+
+        private void MainRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (StartButton.Enabled == true)
+            {
+                WriteStart();
+            }
+            deleteTick = GetCurrentTick() + deleteInterval * 10000;
+        }
+        private void WriteStart()
+        {
+            try
+            {
+                deleteInterval = Int32.Parse(DeleteTimeSetTextBox.Text);
+            }
+            catch
+            {
+                DeleteTimeSetTextBox.Text = "5000";
+                deleteInterval = 5000;
+            }
+
+            StartButton.Enabled = false;
+            deleteTick = GetCurrentTick() + deleteInterval * 10000;
+            DeleteTimeSetTextBox.Enabled = false;
+            TimecheckTimer.Enabled = true;
+        }
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            WriteStart();
+        }
+
+        private void FontSelectButton_Click(object sender, EventArgs e)
+        {
+            fontDialog.ShowColor = true;
+
+            fontDialog.Font = MainRichTextBox.Font;
+            fontDialog.Color = MainRichTextBox.ForeColor;
+
+            if (fontDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                MainRichTextBox.Font = fontDialog.Font;
+                MainRichTextBox.ForeColor = fontDialog.Color;
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            byte[] text_bytes = Encoding.UTF8.GetBytes(MainRichTextBox.Text);
+
+            // Displays a SaveFileDialog so the user can save the Image
+            // assigned to Button2.
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+            saveFileDialog1.Title = "Save a text file";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                System.IO.FileStream fs =
+                    (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the
+                // File type selected in the dialog box.
+                // NOTE that the FilterIndex property is one-based.
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                    case 2:
+                        //userInput.Position = 0;
+                        //userInput.WriteTo(fileStream);
+                        fs.Write(text_bytes, 0, text_bytes.Length);
+                        break;
+                    default:
+                        break;
+                }
+
+                fs.Close();
+            }
+        }
+
+
+    }
+}
